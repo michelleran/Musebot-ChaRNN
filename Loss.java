5807d10bd8e11e00922beec7963bb51de2463277
Michelle Ran <michelleran@piggywheelapps.com>
2018-02-12 18:52:24 -0500

    Forgot Loss.java - added!

---
+++ Loss.java
@@ -0,0 +1,17 @@
+package com.mran.ChaRNN;
+
+public class Loss {
+	public double loss;
+	public double[][] dWxh, dWhh, dWhy;
+	public double[] dbh, dby, state;
+	
+	public Loss(double loss, double[][] dWxh, double[][] dWhh, double[][] dWhy, double[] dbh, double[] dby, double[] state) {
+		this.loss = loss;
+		this.dWxh = dWxh;
+		this.dWhh = dWhh;
+		this.dWhy = dWhy;
+		this.dbh = dbh;
+		this.dby = dby;
+		this.state = state;
+	}
+}
